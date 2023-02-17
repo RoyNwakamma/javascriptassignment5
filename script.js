cc0417f9e4938af53c259d9517139dfc89265284
RoyNwakamma <nwakammaroy0@gmail.com>
2023-02-17 11:55:33 +0100

    javascriptassignment5

---
+++ script.js
@@ -0,0 +1,21 @@
+const myObj = {
+    fname: "Joe",
+    middleName: "Bridget",
+    lname: "Shmoe",
+    age: 45,
+    DOB: "21-2-1980",
+    address: "Vienna",
+    phoneNo: 08131884292,
+    occupation: "Student"
+};
+
+for (let i in myObj) {
+    console.log(`${i}: ${myObj[i]}`);
+}
+
+const myArray = [
+    bridgetAgo = {},
+    royNwakamma = {},
+    rollandUmoh = {},
+    honeyAduma = {}
+];
